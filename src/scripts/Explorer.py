8bbcf4c3b17e3651bbbbe4995955ee0f18c0de5b
westkath <2509480w@student.gla.ac.uk>
2024-03-22 15:49:30 +0000

    Updating Progress and Creating Explorer

---
+++ src/scripts/Explorer.py
@@ -0,0 +1,39 @@
+# Imports
+
+
+# Constants
+DEFAULT_SHIFT = ""
+
+
+# Functions
+def shift_trace(trace, shift=DEFAULT_SHIFT):
+    pass
+
+
+def calculate_footprint(trace):
+    pass
+
+
+def report_summary(results):
+    pass
+
+
+def get_folder_name():  # output/explorer-trace-ci
+    pass
+
+
+# Usage
+# - explorer.py trace_file ci_file config
+def print_usage_exit():
+    usage = ""
+    print(usage)
+    exit(-1)
+
+
+def parse_arguments(arguments):
+    return {}
+
+
+# Main Script
+if __name__ == "__main__":
+    pass
